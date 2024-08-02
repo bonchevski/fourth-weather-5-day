6d501516c67fa9ff4f64cf910b230141f95f4548
bonchevski <victorbonchevski@gmail.com>
2024-08-02 22:08:27 +0300

    fix-deployment issue

--- src/axios/weatherServices/weatherServices.ts
+++ src/axios/weatherServices/weatherServices.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axiosInstance, { API_KEY } from "../axiosInstance";
 
 export const getWeatherByCoordinates = async (latitude: number, longitude: number) => {
     try {
         const response = await axiosInstance.get(`?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
         return response.data;
-    } catch (error) {
+    } catch (error:any) {
         return error.response.data;
     }
 }
@@ -13,7 +14,7 @@ export const getWeatherByCityName = async (cityName: string) => {
     try {
         const response = await axiosInstance.get(`?q=${cityName}&appid=${API_KEY}&units=metric`);
         return response.data;
-    } catch (error) {
+    } catch (error:any) {
         return error.response.data
     }
 }
